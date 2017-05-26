d4ef272c6fe60a7845f7c52ec761d42467e3acac
vaishali lodhi <vililodhi.1994@gmail.com>
2017-05-26 17:17:13 +0530

    node modules.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var config = {
+   entry: ['./src/main.js'
+   ],
+
+   output: {
+      path:'/',
+      filename: 'index.js',
+   },
+
+   devServer: {
+      inline: true,
+      port: 8090
+   },
+
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config
