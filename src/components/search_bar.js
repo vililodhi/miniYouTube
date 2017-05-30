9087a5339801968fde137e0a25a76173425ce45d
vaishali lodhi <vaishali.lodhi@sts.in>
2017-05-30 18:38:24 +0530

    test

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React,{ Component } from 'react';
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
+
+	constructor(props) {
+
+		super(props);
+		this.state = {term: ''};
+	}
+
     render() {
         return (
-			<input />
-		)
-	}
+        	<span>
+    			<input
+    				Value={this.state.term}
+    				onChange={(event) => this.setState({term: event.target.value})}
+    			/>
+    		</span>
+        );
+    }
+
 };
 
 export default SearchBar;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyA_FN0lGmGAtCIVVVWSyTa4L2_lF6M__rw';
+
+class App extends Component {
+
+	constructor(props) {
+
+		super(props);
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'guitar'}, (videos) => {
+			//this.setState({ videos: videos });
+			this.setState({ videos });
+		});
+	}
+
+	render () {
+        return (
+	        <div>
+	        	<div className="col-md-4">
+	        	</div>
+	        	<div className="col-md-4">
+		            <h1>Mini You Tube</h1>
+		            <p><img src="http://i2.kym-cdn.com/photos/images/list/001/246/217/dd4.png" /></ p>
+		            Search : <SearchBar />
+		            <VideoList videos={this.state.videos} />
+		        </div>
+		        <div className="col-md-4">
+		        </div>
+	        </div>
+	    );
+	}
+
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
